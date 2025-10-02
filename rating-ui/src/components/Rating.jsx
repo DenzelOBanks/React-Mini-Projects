15bd7b52265ce06788aa6348aaab73b93bf19713
Denzel <Denzelobanks@gmail.com>
2025-10-02 20:59:31 +0100

    Added star rating Functionality

--- rating-ui/src/components/Rating.jsx
+++ rating-ui/src/components/Rating.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
+
 const Rating = () =>{
-  const stars = Array.from({length:5}, (_, i) => i + 
-  1);
+  
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  const stars = Array.from({length:5}, (_, i) => i + 1);
+  const feedbackMessages = ['Terrible','Poor','Fair','Excelent', 'Amazing']
 
   return ( 
   <div className="rating-container">
     <h2>Rate Your Expericence</h2>
     <div className="star">
       {stars.map((star) => (
-        <span key={star} className="star">
+        <span
+        onMouseEnter={() => setHover(star)} 
+        onMouseLeave={() => setHover(0)} 
+        onClick={() => setRating(star)} key={star} 
+        className={`star ${star <= (hover || rating ) ? 'active' :
+          ''}`}>  
           {'\u2605'}
         </span>
-      )) }
+      ))}
     </div>
+    {rating > 0 && <p className ='feedback' >{ feedbackMessages[rating
+    - 1] }</p>}
   </div> )
 }
 
